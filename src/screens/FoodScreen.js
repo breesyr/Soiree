8001fd0c7dbfeb9e8cb56f87fc341494ad2fd87b
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-08-30 17:51:49 -0700

    Update FoodScreen.js

--- src/screens/FoodScreen.js
+++ src/screens/FoodScreen.js
@@ -1,18 +1,48 @@
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import FoodCard from '../components/FoodCard';
+import SwipeableImage from '../components/SwipeableImage';
+import BottomButtons from '../components/BottomButtons';
+import Swipes from '../components/Swipes';
 
-export default FoodScreen = () => {
+const FoodScreen = () => {
+
     return(
         <View style={styles.container}>
-            <Text>Food Screens</Text>
+            <View style={styles.swipes}>
+                <Swipes/>
+                <BottomButtons/>
+            </View>
+            {/* <FoodCard/> */}
+
         </View>
-    );
+    )
+
 }
 
 const styles = StyleSheet.create({
+
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex : 1,
+        marginTop: 80,
+        paddingLeft: 70,
+        paddingRight: 70
+
     },
-});
+
+    swipes: {
+        flex: 1,
+        padding: 10,
+        paddingTop: 8,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7
+    }
+})
+
+export default FoodScreen;
