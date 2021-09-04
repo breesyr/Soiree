01892cc007ca02dde050a9f1a167108f8f3b4f8b
Sandra Chavez <chavezsandra0498@gmail.com>
2021-09-03 20:33:37 -0700

    file with dummy data

---
+++ Data.js
@@ -0,0 +1,23 @@
+export const foods = [
+    {
+        foodId: 1,
+        title: 'Sushi',
+        photo_url: 
+        'https://cdn.vox-cdn.com/thumbor/KMYC-k9cEynXaPnPeusR8hRsbpk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22469677/Nobu_Restaurant_sushi.jpg',
+        stars: 4
+    },
+    {
+        foodId: 2,
+        title: 'Poke',
+        photo_url: 
+        'https://www.carolynscooking.com/wp-content/uploads/2021/01/Poke-Bowl-18-2.jpg',
+        stars: 4
+    },
+    {
+        foodId: 3,
+        title: 'Spaghetti',
+        photo_url: 
+        'https://www.cookingclassy.com/wp-content/uploads/2012/11/spaghetti+with+meat+sauce11.jpg',
+        stars: 3 
+    }
+]
