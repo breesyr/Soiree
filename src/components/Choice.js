e0aa0dea71656ba122f856128115313c2a518bf1
Sandra Chavez <chavezsandra0498@gmail.com>
2021-09-03 20:31:28 -0700

    Create Choice.js

---
+++ src/components/Choice.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { COLORS } from '../utils/constants'
+
+export default function Choice({type}) {
+    const color = COLORS[type];
+    return (
+        <View style={[styles.container, {borderColor: color}]} >
+            <Text style={[styles.text, {color: color}]}>{type}</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+
+        borderWidth: 7,
+        paddingHorizontal: 15,
+        borderRadius: 15,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+    },
+    text: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        letterSpacing: 4
+
+    }
+})
