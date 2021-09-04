2acfab8a1e7f51089189a28c7a634493df1f32d5
Sandra Chavez <chavezsandra0498@gmail.com>
2021-09-03 20:31:09 -0700

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+const {Dimensions} = require('react-native')
+
+const {width, height} = Dimensions.get('screen')
+
+
+export const FOODCARD = {
+    WIDTH: width * 0.9,
+    HEIGHT: height * 0.7,
+    BORDER_RADIUS: 20,
+    OUT_OF_SCREEN: width + 0.5 * width,
+}; 
+
+
+export const COLORS = {
+    like: '#00eda6',
+    nope: '#ff006f', 
+    comment: "#7f7f7f"
+};
+
+export const ACTION_OFFSET = 100; 
