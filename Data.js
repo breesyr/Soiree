8d5d872873ba2948481a0f9f39e7e204033da235
Paul Nguon <35973829+herropaul@users.noreply.github.com>
2021-09-07 16:53:37 -0700

    Add files via upload

--- Data.js
+++ Data.js
@@ -21,3 +21,21 @@ export const foods = [
         stars: 3 
     }
 ]
+
+export const activities = [
+    {
+        activityId: 1,
+        title: 'Knotts Berry Farm',
+        photo_url:
+        'https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/h6ac5lhcjq6xahxh11rx/knotts-berry-farm?fimg-ssr',
+        stars: 4,
+    },
+    {
+        activityId: 2,
+        title: 'Disneyland',
+        photo_url:
+        'https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/disneyland/attractions/disneyland/sleeping-beauty-castle-walkthrough/sleeping-beauty-castle-exterior-16x9.jpg?1593556896598',
+        stars: 4,
+    }
+
+]
