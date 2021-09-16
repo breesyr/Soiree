7784fa4aebee9b83f1cb6c9061599700ec47b81a
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-09-15 21:14:23 -0700

    Update FoodCard.js

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,87 +1,191 @@
+import React, {useCallback}from 'react'
+import { View, Text, Image, StyleSheet, Animated, TouchableOpacity } from 'react-native'
+import { ACTION_OFFSET, FOODCARD } from '../utils/constants'
+import { LinearGradient } from 'expo-linear-gradient'
+import Choice from './Choice'
+import {Ionicons} from '@expo/vector-icons'
 
-import { View, Text, StyleSheet , Dimensions, TouchableOpacity, ImageBackground} from "react-native";
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
+export default function FoodCard({foodId, title, photo_url, stars, isFirst, swipe, tiltSign, ...rest}) {
+
+    
+    const rotate= Animated.multiply(swipe.x, tiltSign).interpolate({
+        inputRange: [-ACTION_OFFSET, 0, ACTION_OFFSET],
+        outputRange: ['8deg', '0deg', '-8deg']
+    });
+
+    const likeOpacity = swipe.x.interpolate({
+        inputRange: [25, ACTION_OFFSET],
+        outputRange: [0, 1],
+        extrapolate: 'clamp'
+
+    });
+    const nopeOpacity = swipe.x.interpolate({
+        inputRange: [-ACTION_OFFSET, -25],
+        outputRange: [1, 0],
+        extrapolate: 'clamp'
+
+    });
+
+    const animatedCardStyle = {
+        transform: [...swipe.getTranslateTransform(), {rotate}]
+    };
+
+    // const renderStars = useCallback( () => {
+
+    //     let StarArray = []
+    //     for (let x = 1; x <= stars; x++){
+    //         StarArray.push(
+    //             <TouchableWithoutFeedback key={x}>
+    //                 <FilledStar/>
+    //             </TouchableWithoutFeedback>
+    //         )
+    //     };
+
+    //     let remaining = 5 - stars
+    //     for (let x = 1; x <= remaining; x++){
+    //         StarArray.push(
+    //         <TouchableWithoutFeedback key={x}> 
+    //             <EmptyStar/>
+    //         </TouchableWithoutFeedback>
+
+    //         )
+    //     };
 
 
-const FoodCard = () => {
+    //     return(
+    //         <View>
+    //             <View style={styles.starRow}>{StarArray}</View>
+    //         </View>
+    //     )
+    // })
+
+    const renderChoice = useCallback( () => {
+        return (
+        <>
+        <Animated.View style={[styles.likeContainer, {opacity: likeOpacity}]}>
+            <Choice type="like"/>
+        </Animated.View>
+
+
+        <Animated.View style={[styles.dislikeContainer, {opacity: nopeOpacity}]}>
+            <Choice type="nope"/>
+        </Animated.View>
+        </>
+        )
+
+    }, [likeOpacity, nopeOpacity]);
+
+
+
     return (
-        <View style={styles.cardContainer}>
-            <ImageBackground
-            style={styles.imageStyle} 
-            source={require('../../assets/food.jpeg')}>
-                <View style={styles.starContainer}>
-                    <Ionicons name="star"  style={styles.star} size={40} color={'yellow'} > </Ionicons>
-                    <Ionicons name="star" style={styles.star} size={40} color={'yellow'}> </Ionicons>
-                    <Ionicons name="star" style={styles.star} size={40} color={'yellow'}> </Ionicons>
-                    <Ionicons name="star" style={styles.star} size={40}color={'yellow'}> </Ionicons>
-                    <Ionicons name="star" style={styles.star} size={40}color={'white'}> </Ionicons>
-                </View>
-                <View  style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text> Info</Text>
-                    </TouchableOpacity>
+        <Animated.View style={[styles.container, isFirst && animatedCardStyle]} {...rest}>
+            <Image source={{uri: photo_url}} style={styles.image}/>
+            {/* <LinearGradient colors={['transparent', 'rgba(0,0,0,0.9)']} style={styles.gradient}/> */}
+            <Text style={styles.title}>{title}</Text>
+            <TouchableOpacity style={styles.buttonContainer} >
+                <Text style={styles.buttonText}> Info</Text>
+                <Ionicons name="information-circle-outline" color={'white'} size={20} style={{marginLeft: 15}}/>
+            </TouchableOpacity>
 
-                </View>
-            </ImageBackground>
-        </View>
-    );
+            {/* {
+                renderStars()
 
-};
+            } */}
+            <View style={styles.starRow} >
+                    <Ionicons name="star"   size={30} color={'yellow'} > </Ionicons>
+                    <Ionicons name="star"  size={30} color={'yellow'}> </Ionicons>
+                    <Ionicons name="star"  size={30} color={'yellow'}> </Ionicons>
+                    <Ionicons name="star" size={30} color={'yellow'}> </Ionicons>
+                    <Ionicons name="star" size={30} color={'white'}> </Ionicons>
+            </View>
+            <LinearGradient colors={['transparent', 'rgba(0,0,0,0.5)']} style={styles.gradient}/>
 
-const deviceWidth = Math.round(Dimensions.get('window').width) - 30;
+            {
+                isFirst && renderChoice()
+            }
+        </Animated.View>
+    )
+}
+
+
 const styles = StyleSheet.create({
-    cardContainer: {
-        width: deviceWidth,
-        backgroundColor: 'blue',
-        height: 400,
-        borderRadius: 20,
-        // alignContent: 'center',
-        // textAlign: 'center', 
-        alignItems: 'center',
-        justifyContent: 'center',
-        display: 'flex',
-        flexDirection: 'column'
-        
+    container: {
+        position: 'absolute',
+        top: 85, 
 
     },
-    starContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        marginTop:  300,
-        bottom: 0,
-        marginLeft: 20
-        
+    image: {
+        width: FOODCARD.WIDTH,
+        height: FOODCARD.HEIGHT,
+        borderRadius: FOODCARD.BORDER_RADIUS
     },
-    star: {
-        
+    title: {
+        position: 'absolute',
+        bottom: 80,
+        left: 20,
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#fff'
+
+    },
+    gradient: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 160,
+        borderRadius: FOODCARD.BORDER_RADIUS
+
     },
     buttonContainer: {
-        flex: 10
+        position: 'absolute',
+        flexDirection: 'row',
+        alignItems: 'center',
+        bottom: 60,
+        left: 20,
     },
-    imageStyle: {
-        height: 400,
-        borderRadius: 30,
-        width: deviceWidth,
-        opacity: 0.85
+    buttonText: {
+        fontWeight: 'bold',
+        color: '#fff',
+        fontSize: 20
 
     }, 
-    button: {
-        height: 50,
-        backgroundColor: 'gray',
-        marginTop: 'auto',
+    starRow: {
+        position: 'absolute',
+        bottom: 30,
+        left: 20,
+
+        flexDirection: 'row',
         alignItems: 'center',
-        textAlign: 'center',
-        justifyContent: 'center',
-        marginLeft: 70,
-        width: '50%',
-        color: 'black',
-        borderRadius: 20
-       
+    },
+    // choiceContainer:{
+    //     position: 'absolute',
+    //     top: 100
+    // },
+    likeContainer: {
+        position: 'absolute',
+        top: 100,
+        left: 45,
+        transform: [{rotate: '-30deg'}]
+    },
+    dislikeContainer: {
+        position: 'absolute',
+        top: 100,
+        right: 45, 
+        transform: [{rotate: '30deg'}]
     }
+})
 
+
+class FilledStar extends React.Component {
+
+    render(){
+        return <Ionicons name="star"   size={30} color={'yellow'} > </Ionicons>
     }
-);
+}
 
-
-export default FoodCard;
+class EmptyStar extends React.Component {
+    render(){
+        return <Ionicons name="star"   size={30} color={'white'} > </Ionicons>
+    }
+}
