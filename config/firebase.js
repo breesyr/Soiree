0badee625031faaa74a73a02162e888b5c22e0b7
abstractprototype <cheayuenchen@gmail.com>
2021-09-15 17:35:44 -0700

    config

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDsLmTFKV_VE86pq20OO5IjwTE98rPSDVY",
+  authDomain: "soiree-b1c19.firebaseapp.com",
+  projectId: "soiree-b1c19",
+  storageBucket: "soiree-b1c19.appspot.com",
+  messagingSenderId: "196133597750",
+  appId: "1:196133597750:web:e8c03358b5f53bc794de97",
+  measurementId: "G-MYR8YB0ZFW"
+}
