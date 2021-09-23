9c2967c14175ff79c90702694bdf1668b6a4d5b9
Paul Nguon <35973829+herropaul@users.noreply.github.com>
2021-09-23 16:03:10 -0700

    Updated button icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,15 @@ import { COLORS } from '../utils/constants'
 export default function Footer({handleChoice}) {
     return (
         <View style={styles.container}>
-            <Buttons name="thumbs-down" size={30} color={COLORS.nope} onPress={() => handleChoice(-1)} />
-            <Buttons name="comment" size={30}  color={COLORS.comment} />
-            <Buttons name="thumbs-up" size={30} color={COLORS.like} onPress={() => handleChoice(1)}/>
+            <View style={styles.thumbsDown}>
+                <Buttons name="thumbs-down" size={30} color={COLORS.nope} onPress={() => handleChoice(-1)} />
+            </View>
+            <View style={styles.comment}>
+                <Buttons name="comment" size={30}  color={COLORS.comment} />
+            </View>
+            <View style={styles.nope}>
+                <Buttons name="thumbs-up" size={30} color={COLORS.like} onPress={() => handleChoice(1)}/>
+            </View>
         </View>
     )
 }
@@ -18,11 +24,30 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         position: 'absolute',
-        bottom: 15,
+        bottom: 25,
         width: 170,
         alignItems: 'center',
         justifyContent: 'space-between',
-        zIndex: -1
-
+    },
+    thumbsDown: {
+        width: 60,
+        height: 60,
+        borderRadius: 40,
+        borderColor: '#ff006f',
+        borderWidth: 3,
+    },
+    comment: {
+        width: 60,
+        height: 60,
+        borderRadius: 40,
+        borderColor: '#7f7f7f',
+        borderWidth: 3,
+    },
+    nope: {
+        width: 60,
+        height: 60,
+        borderRadius: 40,
+        borderColor: '#00eda6',
+        borderWidth: 3,
     }
 })
