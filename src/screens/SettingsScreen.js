67f91ee928720083c9dbe0c6e0553f8736646015
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-10-07 23:58:48 -0700

    Update SettingsScreen.js

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -21,29 +21,61 @@ export default SettingsScreen = () => {
     };
     return(
         <View style={styles.container}>
+            <View style={styles.innerContainer}>
             <Image source={{uri: "https://pbs.twimg.com/media/EfJCviuVAAANPh0.jpg"}}
             style={styles.image}/>
-            <View style={styles.button}>
+            <View>
+                <Text style={styles.username}>Username</Text>
+                <Text>Edit Profile</Text>
+            </View>
+            <View style={{ marginLeft: 'auto'}}> 
+            <FontAwesome5 name="arrow-right" size={20} />
+            </View>
+
+            </View>
+
+
+            {/* <View style={styles.button}>
                 <FontAwesome5 name="user" size={30}/> 
                 <Text style={styles.buttonText}>Username</Text>
                 </View>
             <View style={styles.button}>
                 <FontAwesome5 name="envelope" size={30}/>
-                <Text style={styles.buttonText}>Email</Text></View>
+                <Text style={styles.buttonText}>Email</Text></View> */}
 
             <TouchableOpacity style={styles.button}>
-                <FontAwesome5 name="user-circle" size={30}/> 
-                <Text style={styles.buttonText}>Account</Text></TouchableOpacity>
+                <FontAwesome5 name="user-circle" size={30} style={{marginRight: 10}}/> 
+                <Text style={styles.buttonText}>Edit Account</Text>
+                <View style={{ marginLeft: 'auto'}}> 
+                <FontAwesome5 name="arrow-right" size={30} />
+                </View>
+            </TouchableOpacity>
 
             <TouchableOpacity style={styles.button}>
                 <FontAwesome5 name="bell" size={30}/>
-                <Text style={styles.buttonText}>Notifications</Text></TouchableOpacity>
+                <Text style={styles.buttonText}>Notifications</Text>
+                <View style={{ marginLeft: 'auto'}}> 
+                <FontAwesome5 name="arrow-right" size={30} />
+                </View>
+                </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+                <FontAwesome5 name="user-shield" size={30}/>
+                <Text style={styles.buttonText}>Security</Text>
+                <View style={{ marginLeft: 'auto'}}> 
+                <FontAwesome5 name="arrow-right" size={30} />
+                </View>
+                </TouchableOpacity>
 
             <TouchableOpacity
                 onPress={onPress}
                 style={styles.button}>
                 <FontAwesome5 name="door-open" size={30}/>
-                <Text style={styles.buttonText}>Log out</Text></TouchableOpacity>
+                <Text style={styles.buttonText}>Log out</Text>
+                <View style={{ marginLeft: 'auto'}}> 
+                <FontAwesome5 name="arrow-right" size={30} />
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -56,18 +88,35 @@ const styles = StyleSheet.create({
         // alignItems: 'center',
         // justifyContent: 'center',
     },
+    innerContainer: {
+        top: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20
+    },
     image: {
         top: 25,
-        height: 200,
-        width: 200,
-        borderRadius: 100,
-        marginBottom: 50
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        marginBottom: 50,
+        marginRight: 15
+    },
+    username:{
+        fontWeight: "500",
+        fontSize: 15,
+        color: '#808080'
+    },
+    arrow: {
+        position: 'absolute',
+
     },
     button: {
         flexDirection: 'row',
-        borderBottomWidth: 3,
-        borderBottomColor: 'black',
-        marginBottom: 5
+        borderBottomWidth: 2,
+        // borderBottomColor: 'black',
+        marginBottom: 25
     },
     buttonText: {
         fontSize: 30
