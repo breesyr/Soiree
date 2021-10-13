88fd2adee7eb27fda0fe5a89f74e147eff6c29d9
Paul Nguon <35973829+herropaul@users.noreply.github.com>
2021-10-13 12:59:27 -0700

    Added yelp_api directory
    contains api key, and get method for accessing the database

---
+++ yelp_api/config.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'https://api.yelp.com/v3/businesses/search?';
+// https://cors-anywhere.herokuapp.com/
+// location=NYC&categories=bars&open_now=true'
+
+export const BEARER_TOKEN = '7MikLsdlhyGpBbIGEV_f5keIt5ap_gdzMzDCPYfKjPbkHzWX5fIhaS8V3YOI-l-BbHRpfUi7X8r6uELCsegZnwgXIYaddz6U1TJx2yEBRXlrxm_lYPXzRnnE8TJWYXYx';
+
+//export const BEARER_TOKEN = 'urjMpuswlc6EKXlkGiCWUw';
