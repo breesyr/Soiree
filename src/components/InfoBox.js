ad39e4df73923ed7da0583e6fc8f2b928eb340d4
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-10-21 02:21:04 -0700

    Update InfoBox.js

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { INFOBOX } from '../utils/constants'
 
 export default function InfoBox() {
     return (
         <View style={styles.container}>
             <View style={styles.box} >
                 <View style={styles.titleBox}>
-                <Text style={styles.title}>Info</Text>
+                <Text style={styles.title}>I n f o</Text>
                 </View>
 
                 <View style={{padding: 30, marginTop: 5}}>
                     <Text style={{fontStyle: 'italic'}}>
-                    sushi, a staple rice dish of Japanese cuisine, 
-                    consisting of cooked rice flavoured with vinegar 
-                    and a variety of vegetable, egg, or raw seafood garnishes and served cold. 
+                    This is just a sample description in the meantime.
+                    
                     </Text>
                 </View>
             </View>
@@ -25,23 +25,26 @@ const styles = StyleSheet.create({
     container: {
 
         alignItems: 'center',
-        margin: 100
+        margin: 50,
+        position: 'relative',
 
     },
     box: {
-        backgroundColor: 'white',
-        height: 200,
-        width: 350, 
+        backgroundColor: '#E1D5E7',
+        height: INFOBOX.HEIGHT,
+        width: INFOBOX.WIDTH, 
         flexDirection: 'row',
-        borderRadius: 250,
-        position: 'absolute'
+        borderRadius: INFOBOX.BORDER_RADIUS,
+        position: 'absolute',
+        borderWidth: 5,
+        borderColor: "#7961c2"
         
     },
     titleBox: { 
         position: 'absolute', 
         width: 300, 
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
 
     title: {
