1f6ec2e1c346fc743cfea4e9bc1c201d1922aa53
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-11-01 19:08:32 -0700

    Update InfoScreen.js

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, TouchableOpacity, Image} from 'react-native'
 import InfoBox from '../components/InfoBox'
 import {FontAwesome5} from '@expo/vector-icons' 
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import {pictures} from '../../Data.js'
+const {Dimensions} = require('react-native')
+
 
 export default function InfoScreen({navigation}) {
+    const { width } = Dimensions.get('window');
+    const [indexSelected, setIndexSelected] = useState(0);
+
+    const onSelect = indexSelected => {
+        setIndexSelected(indexSelected);
+    }
+
     return (
         <View style={{backgroundColor: '#E1D5E7', flex: 2}} >
         <View style={{marginTop: 50, flexDirection: 'row'}}>
@@ -18,7 +29,33 @@ export default function InfoScreen({navigation}) {
                 <Text style={{fontStyle: 'italic', fontSize: 20}}>close</Text>
             </View>
         </View>
+            <View style={{ height: 250,  marginTop: 20 }}>
+                <Carousel
+                    layout='default'
+                    data={pictures}
+                    sliderWidth={width}
+                    itemWidth={width}
+                    onSnapToItem={index => onSelect(index)}
+                    renderItem={({ item, index }) => (
+                    <Image
+                        key={index}
+                        style={{ width: '100%', height: '100%' }}
+                        resizeMode='contain'
+                        source={{uri : item.url}}
+                    />
+                    )}
+                />
+                  <Pagination
+                    inactiveDotColor='gray'
+                    dotColor={'orange'}
+                    activeDotIndex={indexSelected}
+                    dotsLength={pictures.length}
+                    animatedDuration={150}
+                    inactiveDotScale={1}
+                />
+            </View>
         <InfoBox/>
+
     </View>
     )
 }
