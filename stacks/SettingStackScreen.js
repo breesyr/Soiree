b047157e7828734e87a4c2cda9ab604ce5a18644
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2021-11-18 23:43:20 -0800

    Update SettingStackScreen.js

--- stacks/SettingStackScreen.js
+++ stacks/SettingStackScreen.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import SettingsScreen from "../src/screens/SettingsScreen";
 import AccountScreen from "../src/screens/AccountScreen";
+import PreferenceScreen from "../src/screens/PreferenceScreen";
 
 export default SettingStackScreen = () => {
     const SettingStack = createStackNavigator();
@@ -11,6 +12,7 @@ export default SettingStackScreen = () => {
         <SettingStack.Navigator screenOptions={{ headerShown: false }}>
             <SettingStack.Screen name = "Settings" component = {SettingsScreen}/>
             <SettingStack.Screen name = "Account" component = {AccountScreen}/>
+            <SettingStack.Screen name = "Preference" component = {PreferenceScreen}/>
         </SettingStack.Navigator>
     ); 
 };
